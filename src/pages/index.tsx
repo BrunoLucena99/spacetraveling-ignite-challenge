6c788171078c56f5f71efcd6d18c72c81562405f
Bruno <brunohenriquelucena@gmail.com>
2021-11-18 18:40:06 -0300

    feat: home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,123 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={styles.mainContainer}>
+        <Header />
+        <div className={styles.posts}>
+          <a>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sicronização em vez de ciclos de vida</p>
+            <div className={styles.postInfo}>
+              <div>
+                <FiCalendar />
+                <time>19 de Abril 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Danilo Vieira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Criando um app CRA do zero</strong>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div className={styles.postInfo}>
+              <div>
+                <FiCalendar />
+                <time>16 de Abril 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Joseph Vieira</span>
+              </div>
+            </div>
+          </a>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+          <a>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sicronização em vez de ciclos de vida</p>
+            <div className={styles.postInfo}>
+              <div>
+                <FiCalendar />
+                <time>19 de Abril 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Danilo Vieira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Criando um app CRA do zero</strong>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div className={styles.postInfo}>
+              <div>
+                <FiCalendar />
+                <time>16 de Abril 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Joseph Vieira</span>
+              </div>
+            </div>
+          </a>
 
-//   // TODO
-// };
+          <a>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sicronização em vez de ciclos de vida</p>
+            <div className={styles.postInfo}>
+              <div>
+                <FiCalendar />
+                <time>19 de Abril 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Danilo Vieira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Criando um app CRA do zero</strong>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div className={styles.postInfo}>
+              <div>
+                <FiCalendar />
+                <time>16 de Abril 2021</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>Joseph Vieira</span>
+              </div>
+            </div>
+          </a>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'post')
+  );
+
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  return {
+    props: {},
+  };
+};
